fadda4b6dd16937eb4eec92264f57ae7cbd6dd70
Koen Vossen <koen@teamtv.nl>
2019-06-21 17:20:12 +0200

    Add some more info to goals

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useStat } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { EventStream, PollingEventStreamSource } from "@teamtv/eventstream-client";
 
@@ -17,9 +17,20 @@ const debounce = (func, wait, immediate) => {
   };
 };
 
+const periodState = (periodEvents) => {
+  const eventTypes = periodEvents.map(({eventType}) => eventType);
+  if (eventTypes.indexOf("endPeriod") !== -1) {
+    return "ENDED";
+  } else if (eventTypes.indexOf("startPeriod") !== -1) {
+    return "STARTED";
+  } else {
+    return "NOT-STARTED";
+  }
+};
+
 const statsCollector = (eventLog, type, preCalculated) => {
   switch (type) {
-    case 'match':
+    case 'match': {
       const createdEvents = eventLog.filter(({eventType}) => eventType === "sportingEventCreated");
       if (createdEvents.length === 1) {
         return {
@@ -30,20 +41,52 @@ const statsCollector = (eventLog, type, preCalculated) => {
       } else {
         return null;
       }
-    case 'score':
+    }
+    case 'score': {
       const match = preCalculated.match || statsCollector(eventLog, 'match');
-      const goals = eventLog.filter(({eventType}) => eventType === "goal" || eventType === "goalCorrection");
+      const goals = eventLog.filter(({eventType, result}) => (eventType === "shot" && result === "GOAL") || eventType === "goalCorrection");
       return {
         home: goals.filter(({teamId}) => teamId === match.homeTeam.teamId).length,
         away: goals.filter(({teamId}) => teamId === match.awayTeam.teamId).length,
       };
+    }
+    case 'period': {
+      const periodEvents = eventLog.filter(({eventType}) => eventType === "startPeriod" || eventType === "endPeriod");
+      return {
+        period1: periodState(periodEvents.filter(({period}) => period == '1')),
+        period2: periodState(periodEvents.filter(({period}) => period == '2'))
+      };
+    }
+    case 'goals': {
+      const match = preCalculated.match || statsCollector(eventLog, 'match');
+      const score = {home: 0, away: 0};
+      return eventLog.filter(
+        ({eventType, result}) => (eventType === "shot" && result === "GOAL") || eventType === "goalCorrection"
+      ).map((goal) => {
+        if (goal.teamId === match.homeTeam.teamId) {
+          score.home += 1;
+        } else {
+          score.away += 1;
+        }
+        return {
+          score: {home: score.home, away: score.away}, // copy
+          team: goal.teamId === match.homeTeam.teamId ? match.homeTeam : match.awayTeam,
+          ...goal
+        };
+      });
+    }
+    case 'shots':
+      return eventLog.filter(({eventType}) => eventType === "shot");
+    case 'raw':
+      return eventLog.slice();
+
   }
 };
 
 const Context = React.createContext([]);
 
 const StatsProvider = ({endpointUrl, children}) => {
-  const [eventLog, setEventLog] = React.useState([]);
+  const [eventLog, setEventLog] = useState([]);
 
   useEffect(() => {
     setEventLog([]);
@@ -58,8 +101,7 @@ const StatsProvider = ({endpointUrl, children}) => {
 
     const addEvent = (event) => {
       if (event.eventType === "removed") {
-        if (!!event.id)
-        {
+        if (!!event.id) {
           const index = _eventLog.findIndex(({id}) => id === event.id);
           if (index !== -1) {
             _eventLog.splice(index, 1);
@@ -77,9 +119,7 @@ const StatsProvider = ({endpointUrl, children}) => {
     });
 
     eventStream.on("shot", ({id, time, person, result, type, possession: {teamId}}) => {
-      if (result === "GOAL") {
-        addEvent({id, eventType: "goal", teamId, time, person, type});
-      }
+      addEvent({id, eventType: "shot", result, teamId, time, person, type});
     });
     eventStream.on("goalCorrection", ({id, teamId}) => {
       addEvent({eventType: "goalCorrection", id, teamId});
@@ -88,7 +128,7 @@ const StatsProvider = ({endpointUrl, children}) => {
       addEvent({eventType: "startPeriod", period});
     });
     eventStream.on("endPeriod", ({period}) => {
-      addEvent({eventType: "startPeriod", period});
+      addEvent({eventType: "endPeriod", period});
     });
     eventStream.on("observationRemoved", ({id}) => {
       addEvent({eventType: "removed", id});
@@ -115,7 +155,7 @@ const StatsConsumer = ({types, children}) => {
             match: statsCollector(eventLog, 'match'),
           };
           if (stats.match) {
-            for(const statsType of types) {
+            for (const statsType of types) {
               stats[statsType] = statsCollector(eventLog, statsType, stats);
             }
           }
